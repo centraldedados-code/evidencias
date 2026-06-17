60fb42fe452bc6155c3ed13b77f3166c667082ac
centraldedados-code <centraldedados@institutobrarte.org>
2026-06-17 20:46:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // ============================================================
 // CONFIGURAÇÃO — ALTERE A URL ABAIXO APÓS PUBLICAR O WEB APP
 // ============================================================
-var WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyjB5Wc6iKplH33CBPuqEGmHhseyfkm2S5PBrbO092_AawJalh43zp947dAVz3ldJxg/exec';
+var WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbxMxMOH5zLmi-wjL8y9N91L9kFuVIz3bDhJu-vGDGkf4W3AUW5hJoBsSh7POCKoiZRgJQ/exec';
 // Exemplo: 'https://script.google.com/macros/s/AKfy.../exec'
 
 
